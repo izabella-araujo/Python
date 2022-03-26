d7f3c4507bd6eccaca0f808e4fa4c0f63f36b2b4
unknown <mariaizabellasda@gmail.com>
2022-03-26 15:21:31 -0300

    progressão aritmética

---
+++ ex057.py
@@ -0,0 +1,8 @@
+a=int(input('Qual o primeiro termo ds PA?'))
+b=int(input('Qual a razão da PA?'))
+termo=a
+cont=0
+while cont>=10:
+    print=('{}'.format(termo, end=''))
+    termo+=b
+    cont+=1
