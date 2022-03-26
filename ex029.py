75094f060202e5b5d2e410df5efa3251c6bae402
unknown <mariaizabellasda@gmail.com>
2022-03-26 13:19:34 -0300

    ler um ano e diz se é bissexto

---
+++ ex029.py
@@ -0,0 +1,7 @@
+a=int(input('Digite um anoe descubra se é bissexto:'))
+if a%4==0 and a %100!=0 or a%400==0:
+    print('O ano é bissexxto')
+else:
+    print('O ano não é bissexto')
+print('--FIM--')
+
