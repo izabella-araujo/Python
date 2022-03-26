3fbe0dfa38f202ba2524909118cb425027b25851
unknown <mariaizabellasda@gmail.com>
2022-03-26 14:55:19 -0300

    contagem regressiva

---
+++ ex042.py
@@ -0,0 +1,5 @@
+from time import sleep
+for cont in range(10, -1, -1): #coloca o  primeiro -1p pq o range ignora o ultimo e o segundo -1 pq é o passo#
+    print(cont)
+    sleep(1.0)
+print('BUUUUM!')
