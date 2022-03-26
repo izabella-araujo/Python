5977c12d9aed5ab9a07c44561e4256e63f0bb3f0
unknown <mariaizabellasda@gmail.com>
2022-03-26 12:34:26 -0300

    ler a largura e altura da parede e calcula a quantidade de tinta para pintar

---
+++ ex010.py
@@ -0,0 +1,7 @@
+a=int(input('Qual a largura da parede?'))
+b=int(input('Qual a altura da parede?'))
+area=a*b
+tinta=(area*1)/2
+print('A área da parede é:', area)
+print('A quantidade de tinta necessaria é:', tinta, 'litros')
+
